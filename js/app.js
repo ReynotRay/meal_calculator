aea4672d2aba8558f6045c78d75507d9cb2acb97
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-06-30 16:51:32 -0500

    edit

--- js/app.js
+++ js/app.js
@@ -1,126 +1,136 @@
+    
+    var menuPrices = (function() {
+        var prices = {
+            "Hamburger": 6.95,
+            "Hot Dog": 2.95,
+            "Chicken Burger": 5.95,
+            "Veggie Sandwich": 4.95,
+            "Cheeseburger": 7.95,
+            "Bratwurst": 1.95,
+            "Fries": 1.95,
+            "Club Wrap": 2.95,
+            "Bacon Burger": 3.95,
+            "Chili Dog": 3.95,
+            "Turkey Sandwich": 9.95,
+            "Pizza": 8.95,
+        };
+        return function(item) {
+            return prices[item];
+        };
+    }());
+    var images = ['hamburger', 'hotdog', 'chickenburger', 'veggie',
+        'cheeseburger', 'bratwurst', 'fries', 'clubwrap', 'bacon-burger',
+        'chilidog', 'turkeysandwich', 'pizza'
+    ];
+    var ext = '.jpeg';
+    var categories = ["Hamburger", "Hot Dog", "Chicken Burger",
+        "Veggie Sandwich", "Cheeseburger", "Bratwurst", "Fries",
+        "Club Wrap", "Bacon Burger", "Chili Dog", "Turkey Sandwich",
+        "Pizza"
+    ];
 
-
-var menuPrices = (function() {
-    var prices = {
-        "Hamburger": 6.95,
-        "Hot Dog": 2.95,
-        "Chicken Burger": 5.95,
-        "Veggie Sandwich": 4.95,
-        "Cheeseburger": 7.95,
-        "Bratwurst": 1.95,
-        "Fries": 1.95,
-        "Club Wrap": 2.95,
-        "Bacon Burger": 3.95,
-        "Chili Dog": 3.95,
-        "Turkey Sandwich": 9.95,
-        "Pizza": 8.95,
-        "Split Bill":0
-    };
-
-    return function(item) {
-        return prices[item];
-    };
-}());
-
-console.log('fucks');
-var totalPrice = (function() {
-    var total = 0;
-    return function(price) {
-        total = total + price;
-        return parseFloat(total).toFixed(2);
-    };
-}());
-
-$(".menu").click(function() {
-    $('#nosale').text('Current Sale');
-
-    if ($(this).text()) {
-        $('.charge').text('Charge $' + totalPrice(menuPrices($(this).text())) + " ");
-        $('.items').append('<p class="item-name">' + $(this).text()+ 
-            '<span class="item-price">$'+ menuPrices($(this).text()) + '<div id="takeaway">Xhere');
+    function generateItems() {
+        for (var i = 0; i < images.length; i++) {
+            $('.thumbnails').append('<li><img src="assets/' + images[i] +  ext + '"><div class ="caption">' + categories[i] + '</span></li>');
+        }
     }
-});
-    //split dinner into 2
-    $("#split").click(function() {
-    $('.charge').append('Charge $' + totalPrice(menuPrices($(this).text())));
-    $('.charge').text('Charge $' + (totalPrice(menuPrices)/2));
+    generateItems();
+    console.log('fucks');
+    var totalPrice = (function() {
+        var total = 0;
+        return function(price) {
+            total = total + price;
+            return parseFloat(total).toFixed(2);
+        };
+    }());
+    $(".thumbnails li").click(function() {
+        $('#nosale').text('Current Sale');
+        console.log("click");
+        if ($(this).text()) {
+            $('.charge').text('Charge $' + totalPrice(menuPrices($(this)
+                .text())) + " ");
+            $('.items').append('<p class="item-name">' + $(this).text() +
+                '<span class="item-price">$' + menuPrices($(this).text())
+            );
+        }
     });
-
-// $("#remove").click(function() {
-//     if ($(this).text()) {
-//         console.log('removed');
-//         $('#nosale').text('No Sale');
-//     $('.sale').css('background','#ff5959');
-//         $( ".items" ).empty();
-//         $('.charge').text('Charge $' + totalPrice(menuPrices($(this).text())) * 0 + " " );
-//         // $('.items').append('<p><h4 class="item-name">' + $(this).text() + '</h4>' + '<span class="item-price">$ ' 
-//         //     + menuPrices($(this).text()) + '</span></p>');
-//     }
-// });
-
-
-//remove dinner items from items div
-	$("#remove").click(function() {
-      $('.charge').text('Charge $' + (totalPrice(menuPrices)*0));
-  	$( ".items").empty();
-  
-  	$('#nosale').text('No Sale');
-    $('.sale-area').css('background','#ff5959');
-});
-
-//when mouse enter arrow display items
-	$(".dropdown").mouseenter(function() {
-  	$('.dropdown-content').fadeIn();
-});
-
-	$(".dropdown").mouseleave(function() {
-  	$('.dropdown-content').fadeOut();
-});
-
-//when there is input of item display Current sale
-	$('.menu').click(function()	{
-	$('#nosale').text('Current Sale');
-    $('.sale-area').css('background','#00cc00');
-    $('#nosale').css('color','white');
-
-});
-//click on drop down arrow
+    //remove dinner items from items div
+    $("#remove").click(function() {
+        $('.charge').text('Charge $' + (totalPrice(menuPrices) * 0));
+        $(".items").empty();
+        $('#nosale').text('No Sale');
+        $('.sale-area').css('background', '#ff5959');
+    });
+    //when mouse enter arrow display items
+    $(".dropdown").mouseenter(function() {
+        $('.dropdown-content').fadeIn();
+    });
+    // $(".dropdown").mouseleave(function() {
+    //     $('.dropdown-content').fadeOut();
+    // });
+    //when there is input of item display Current sale
+    $('.thumbnails li').click(function() {
+        $('#nosale').text('Current Sale');
+        $('.sale-area').css('background', '#00cc00');
+        $('#nosale').css('color', 'white');
+    });
+    //click on drop down arrow
     $('.dropdown').click(function() {
-    $('.dropdown-content').show();
-});
-
+        $('.dropdown-content').show();
+    });
     $('.takeaway').click(function() {
-    console.log('click');
-    $('.item-price').hide();
-    $('.item-name').hide();
+        console.log('click');
+        $('.item-price').hide();
+        $('.item-name').hide();
+    });
+    $(".charge").one('click', function() {
+        $('.thumbnails').hide();
+        $('.sale-area').css('width', '855px');
+        $('#back').css('display', 'block');
+    });
+    $('.charge').click(function() {
+        $('.thumbnails').hide();
+        $('.sale-area').css('width', '855px');
+        $('#back').css('display', 'block');
+    });
+    $('#back').click(function() {
+        $('.thumbnails').fadeIn();
+        $('.sale-area').css('width', '319px');
+    });
+    $('#split').mouseenter(function() {
+        $(".split1").show();
+    });
+    $('#start-over').click(function() {
+        location.reload();
+    });
+    $(".split1").append(
+        '<input type="text" id="split-display" placeholder="How Many?">');
+
+    function divideBetweenMembers(number) {
+        return totalPrice(0) / number;
+        // return parseFloat(total).toFixed(2);
+    }
+    $('#split-display').keydown(function(e) {
+        console.log('cooool');
+        if (e.which === 13) {
+            console.log(divideBetweenMembers($(this).val()));
+            $('.charge').text('$' + " " + divideBetweenMembers($(this).val()) +
+                " " + 'per person');
+            // $('.charge').text('each party will party will pay');
+        }
     });
 
-    $(".charge").one('click', function () { 
-    $(".charge").append('<input type="text" id="split-display" placeholder="Split Bill?">');  
-    $('.column1').hide();
-    $('.column2').hide();
-    $('.column3').hide();
-    $('.sale-area').css('width','855px');
-    $('#back').css('display','block');
-
-});
-$('.charge').click(function() {
-    $('.column1').hide();
-    $('.column2').hide();
-    $('.column3').hide();
-    $('.sale-area').css('width','855px');
-    $('#back').css('display','block');
-});
-$('#back').click(function() {
-    $('.column1').fadeIn();
-    $('.column2').fadeIn();
-    $('.column3').fadeIn();
-    $('.sale-area').css('width','319px');
-    $('#split-display').hide();
-});
-
-$('#start-over').click(function() {
-    location.reload();
-});
-
-
+    function addTax() {
+        return totalPrice(0) * .0825;
+        return 
+    }
+    $('.thumbnails').click(function() {
+        $('.tax').text('tax' + ' ' + '$' + addTax($(this).val()));
+    });
+    // var totalPrice = (function() {
+    //     var total = 0;
+    //     return function(price) {
+    //         total = total + price;
+    //         return parseFloat(total).toFixed(2);
+    //     };
+    // }());
